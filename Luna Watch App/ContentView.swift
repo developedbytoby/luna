39320a1e27fc4632579c56fa918f2b36ade3c56d
Toby Brown <hi@tobyb.dev>
2023-12-27 02:09:10 +0000

    Swift shenanigans!

--- Luna Watch App/ContentView.swift
+++ Luna Watch App/ContentView.swift
@@ -6,17 +6,57 @@
 //
 
 import SwiftUI
+import SwiftOpenAI
+import Foundation
+
+struct Config {
+    static var openAIKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "Config", ofType: "plist") else {
+                fatalError("Couldn't find file 'Config.plist'.")
+            }
+            let plist = NSDictionary(contentsOfFile: filePath)
+            guard let value = plist?.object(forKey: "OpenAI_API_Key") as? String else {
+                fatalError("Couldn't find key 'OpenAI_API_Key' in 'Config.plist'.")
+            }
+            return value
+        }
+    }
+}
+
+var openAI = SwiftOpenAI(apiKey: Config.openAIKey)
 
 struct ContentView: View {
+    @State private var userInput: String = ""
+    @State private var response: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Enter your message", text: $userInput)
+                .padding()
+            Text(response)
+                .padding()
+            
+            Button("Send Message") {
+                sendMessage()
+            }
         }
         .padding()
     }
+}
+
+func sendMessage() {
+    Task {
+        do {
+            for try await newMessage in try await openAI.createChatCompletionsStream(model: .gpt3_5(.turbo),
+                                                                                     messages: [.init(text: "Say hello!", role: .user)],
+                                                                                     optionalParameters: .init(stream: true)) {
+                            response = newMessage.replies.first?.content ?? "No response :("
+                            print("New Message Received: \(newMessage) ")
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
 }
 
 #Preview {
