156d6c1307cfe8eedd03ff5df1a477fd3cba1843
Toby Brown <hi@tobyb.dev>
2023-12-27 01:39:54 +0000

    Initial Commit

---
+++ Luna Watch App/LunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaApp.swift
+//  Luna Watch App
+//
+//  Created by Toby Brown on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Luna_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
